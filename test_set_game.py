6b66ffd09aa5b0d03f9243f62b4cb08238f9265e
geterodyn <geterodyn@users.noreply.github.com>
2019-05-07 18:45:09 +0700

    update test_set

--- test_set_game.py
+++ test_set_game.py
@@ -2,9 +2,9 @@ import pytest
 from set_game import Card, check_set 
 
 def test_set():
-	assert check_set([Card(2,'DIAMOND','STRIPPED','RED'),Card(3,'DIAMOND','OPEN','GREEN'),Card(2,'OVAL','SOLID','PURPLE')]) is True
-	assert check_set([Card(1,'DIAMOND','STRIPPED','RED'),Card(2,'DIAMOND','OPEN','RED'),Card(3,'OVAL','OPEN','PURPLE')]) is True
-	assert check_set([Card(3,'DIAMOND','OPEN','GREEN'),Card(1,'OVAL','OPEN','GREEN'),Card(2,'OVAL','SOLID','PURPLE')]) is True
+	assert check_set([Card(2,'DIAMOND','STRIPPED','RED'),Card(2,'DIAMOND','STRIPPED','RED'),Card(2,'DIAMOND','STRIPPED','RED')]) is True
+	assert check_set([Card(1,'DIAMOND','STRIPPED','RED'),Card(2,'OVAL','OPEN','GREEN'),Card(3,'SQUIGGLE','SOLID','PURPLE')]) is True
+	assert check_set([Card(3,'OVAL','OPEN','GREEN'),Card(3,'OVAL','OPEN','GREEN'),Card(3,'OVAL','OPEN','GREEN')]) is True
 
 	assert check_set([Card(3,'DIAMOND','STRIPPED','GREEN'),Card(3,'DIAMOND','SOLID','RED'),Card(2,'OVAL','STRIPPED','GREEN')]) is False
 	assert check_set([Card(1,'SQUIGGLE','SOLID','PURPLE'),Card(3,'SQUIGGLE','OPEN','GREEN'),Card(3,'DIAMOND','OPEN','GREEN')]) is False
